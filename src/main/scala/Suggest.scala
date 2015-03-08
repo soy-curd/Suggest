76b96a0d45a8b053d6f480adb5ad15b0a6931c70
soy-curd <soycurd1@gmail.com>
2015-03-08 21:59:41 +0900

    add prrofreading function by yahoo service.

--- src/main/scala/Suggest.scala
+++ src/main/scala/Suggest.scala
@@ -1,5 +1,25 @@
+
+import java.net.URLEncoder.encode
+import scala.io.Source
+
 object Suggest {
   def main(args: Array[String]): Unit = {
-    println("Hello!")
+    val appid = args(0)
+    val sentence = args(1)
+
+    println(proof(appid, sentence))
+  }
+
+  def proof(appid:String, sentence:String) = {
+    val yahooKouseiURL = "http://jlp.yahooapis.jp/KouseiService/V1/kousei?"
+    val params = "appid=%s&sentence=%s".format(
+      encutf(appid), encutf(sentence))
+
+    val response = Source.fromURL(yahooKouseiURL + params)
+    response.getLines.toList
+  }
+
+  def encutf(value: String) = {
+    encode(value, "UTF-8")
   }
 }
